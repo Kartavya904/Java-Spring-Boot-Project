8056041f6b72a1d905eccbbae8abcd3a85c71893
Kartavya Singh <kartavya.singh17@yahoo.com>
2024-09-13 21:22:52 -0400

    Added Initial Files

---
+++ src/main/java/Kartavya_Personal_Project/Java_Spring_Boot_Project/JavaSpringBootProjectApplication.java
@@ -0,0 +1,13 @@
+package Kartavya_Personal_Project.Java_Spring_Boot_Project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSpringBootProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaSpringBootProjectApplication.class, args);
+	}
+
+}
